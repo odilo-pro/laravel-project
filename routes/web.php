8f043191edaeb9dae1523b714e5dc8d877e6d374
odilo-pro <your-email@example.com>
2026-04-09 11:59:42 +0200

    first commit

---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+use App\Http\Controllers\UserController;
+
+Route::get('/users', [UserController::class, 'index']);
+
+?>
